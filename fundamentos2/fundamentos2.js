93d04e5a4b077847bb867b66d2a0f81e9e68e80e
angeelgod <zubzero2000@gmail.com>
2020-02-01 13:39:10 -0300

    Finish work on "Fundamentos Basicos 2" assignment

--- fundamentos2/fundamentos2.js
+++ fundamentos2/fundamentos2.js
@@ -87,3 +87,86 @@ for (i=0;i<fn.length;i++){
 	console.log(fn[i])
 }
 //// Ejercicio 7
+function add(x){
+	for (i=1;i<x.length;i+=2){
+		x[i]++;
+	}
+	return x;
+}
+arr=[1,2,3,4,5,6,7,8,9];
+console.log(add(arr));
+//// Ejercicio 8
+function lo(x){
+	l=[];
+	for (i=0;i<x.length;i++){
+		l.push(x[i].length);
+	}
+	for (y=1;y<x.length;y++)
+		x[y]=l[y-1];
+	return x;
+}
+console.log(lo(['hola','uwu','owo','jue']));
+//// Ejercicio 9
+function sum7(x){
+	y=[];
+	for (i=0;i<x.length;i++){
+		y.push(x[i]+7);
+	}
+	return y
+}
+console.log(sum7([0,1,2,3]));
+//// Ejercicio 10
+function invertir(x){
+	for (i=0;i<x.length/2;i++){
+		a=x[i]
+		x[i]=x[x.length-1-i]
+		x[x.length-1-i]=a
+	}
+	return x;
+
+}
+console.log(invertir([1,2,3,4,5,6,7]));
+//// Ejercicio 11
+function neg(x){
+	for (i=0;i<x.length;i++){
+		if (x[i]>0){
+			x[i]=x[i]*-1;
+		}
+	}
+	return x
+}
+console.log(neg([1,-2,-3,4,5,6,-7]))
+//// Ejercicio 12
+function hungry(x){
+	h=[];
+	for (i=0;i<x.length;i++){
+		if (x[i]=="comida"){
+			h.push("yummy");
+		}
+	}
+	if (h.length==0){
+		return "Tengo Hambre"
+	}
+	else {
+		return h
+	}
+}
+console.log(hungry(["hola"]));
+/// Ejercicio 13
+function centro(x){
+	for (i=0;i<x.length/2;i+=2){
+		a=x[i];
+		x[i]=x[x.length-1-i];
+		x[x.length-1-i]=a
+	}
+	return x;
+}
+console.log(centro([1,2,3,4,5,6]));
+/// Ejercicio 14
+function escalar(a,b){
+	for (i=0;i<b.length;i++){
+		b[i]=b[i]*a;
+	}
+	return b;
+}
+console.log(escalar(3,[1,4,2,1]));
