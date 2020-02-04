9f0c920eda76fcc07df5ea1f2f215303b4bcacbf
angeelgod <zubzero2000@gmail.com>
2020-02-04 09:54:50 -0300

    Modify recursive fibonacci excercise Now it contains less code!!

--- fundamentos_intermedio/fundamentos_inter.js
+++ fundamentos_intermedio/fundamentos_inter.js
@@ -95,11 +95,8 @@ function fibonacci(n){
 		return null;
 	}
 	else {
-		if (n==0){
-			return 0;
-		}
-		else if (n==1){
-			return 1;
+		if (n<=1){
+			return n
 		}
 		else {
 			return fibonacci(n-1)+fibonacci(n-2);
